f3c58333f1b166b6af537ffd891f11facd0f5b3a
Shravan Kotturkar <121821325+creatorsrk2@users.noreply.github.com>
2023-12-25 17:44:41 +0530

    Add javascript user inteface for the game

--- rps.js
+++ rps.js
@@ -8,79 +8,77 @@ function getComputerChoice() {
     return compChoice[random];
 }
 
-//function getUserChoice() to get user input
 
-function getPlayerChoice() {
-    let playerChoice = prompt('Choose Rock Paper or Scissors and key it in: ');
-    playerChoice = playerChoice.toLowerCase();
-    return playerChoice;
-}
+let rock = document.querySelector('.rock') 
+let paper = document.querySelector('.paper') 
+let scissors = document.querySelector('.scissors') 
+let roundResult = document.querySelector('.roundResult') 
+let compScr = 0;
+let yourScr = 0;
+let finalResult = document.querySelector('.finalResult') 
 
+rock.addEventListener('click', function() {
+    if (getComputerChoice() == 'paper') {
+        compScr += 1;
+        if (compScr == 5) {
+            finalResult.textContent = 'The computer has won. Reload the page to play again'    
+        }
+        document.querySelector('#compScore').textContent = compScr;
+        roundResult.textContent = 'Rock lost against Paper';
+    }
+    else if (getComputerChoice() == 'scissors') {
+        yourScr += 1;
+        if (yourScr == 5) {
+            finalResult.textContent = 'You have won. Reload the page to play again'
+        }
+        document.querySelector('#yourScore').textContent = yourScr;
+        roundResult.textContent = 'Rock won against Scissors';
+    }
+    else if (getComputerChoice() == 'rock') {
+        roundResult.textContent = 'Rock tied against Rock';
+    }
+})
 
+paper.addEventListener('click', function() {
+    if (getComputerChoice() == 'scissors') {
+        compScr += 1;
+        if (compScr == 5) {
+            finalResult.textContent = 'The computer has won. Reload the page to play again'
+        }
+        document.querySelector('#compScore').textContent = compScr;
+        roundResult.textContent = 'Paper lost against Scissors';
+    }
+    else if (getComputerChoice() == 'rock') {
+        yourScr += 1;
+        if (yourScr == 5) {
+            finalResult.textContent = 'You have won. Reload the page to play again'
+        }
+        document.querySelector('#yourScore').textContent = yourScr;
+        roundResult.textContent = 'Paper won against Rock';
+    }
+    else if (getComputerChoice() == 'paper') {
+        roundResult.textContent = 'Paper tied against Paper';
+    }
+})
 
-//function playRound() to play a round of R P S
-let scorep = 0;
-let scorec = 0;
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        alert ('TIE');
-        return;
+scissors.addEventListener('click', function() {
+    if (getComputerChoice() == 'rock') {
+        compScr += 1;
+        if (compScr == 5) {
+            finalResult.textContent = 'The computer has won. Reload the page to play again'
+        }
+        document.querySelector('#compScore').textContent = compScr;
+        roundResult.textContent = 'Scissors lost against Rock';
     }
-    else if ((playerSelection != 'rock') && (playerSelection != 'paper') && (playerSelection != 'scissors')) {
-        alert ('Wrong entry! You lose this round')
+    else if (getComputerChoice() == 'paper') {
+        yourScr += 1;
+        if (yourScr == 5) {
+            finalResult.textContent = 'You have won. Reload the page to play again'
+        }
+        document.querySelector('#yourScore').textContent = yourScr;
+        roundResult.textContent = 'Scissors won against Paper';
     }
-    else if ((playerSelection == 'paper' && computerSelection == 'rock')) {
-        alert ('You win this round! Paper beats Rock');
-        ++scorep;       
+    else if (getComputerChoice() == 'scissors') {
+        roundResult.textContent = 'Scissors tied against Scissors';
     }
-    else if ((playerSelection == 'scissors' && computerSelection == 'paper')) {
-        alert ('You win this round! Scissors beats Paper');
-        ++scorep;
-    }
-    else if ((playerSelection == 'rock' && computerSelection == 'scissors')) {
-        alert ('You win this round! Rock beats Scissors');
-        ++scorep;
-    }
-    else if ((playerSelection == 'rock' && computerSelection == 'paper')) {
-        alert ('You lose this round! Paper beats Rock');
-        ++scorec;
-    }
-    else if ((playerSelection == 'paper' && computerSelection == 'scissors')) {
-        alert ('You lose this round! Scissors beats Paper');
-        ++scorec;
-    }
-    else if ((playerSelection == 'scissors' && computerSelection == 'rock')) {
-        alert ('You lose this round! Rock beats Scissors');
-        ++scorec;
-    }
-}
-
-function game() {
-    for (let i = 0; i < 5; i++)
-    {
-        playRound(getPlayerChoice(), getComputerChoice());   
-    }
-    if (scorep > scorec)
-    {
-        alert ('You Won the game. Congrats!');
-    }
-    else
-    {
-        alert ('You Lost the game. Better Luck Next Time.');
-    }
-}
-
-console.log(game());
-
-    //takes 2 parameters playerSelection and computerSelection
-    //use prompt() to get user input
-    //returns the winner with message
-    //case-insensitive parameter playerSelection - to input all variations
-    //if a TIE, replay the round
-    //console.log the results to check, and later return the result
-
-//function game() to play function playRound() 5 times
-    //keep track of score for each round
-    //declare winner at the end of 5 rounds
-    
+})
